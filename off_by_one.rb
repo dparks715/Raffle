5b304b64a9f510945707d5c6daee3925dd8fb80a
Daniel Parks <dparks715@yahoo.com>
2017-01-28 17:10:02 -0500

    function converts to arrays, iterates through finding matches

--- off_by_one.rb
+++ off_by_one.rb
@@ -1,10 +1,18 @@
-def off(ticket)
+def off(ticket, winner)
 	#checking the class after using split.  It returns and array
-	ticket = ticket.split("")
-		puts ticket.class
+	ticket_array = ticket.split("")
+	winner_array = winner.split("")
 	
-	
+	counter = 0
+	matches = 0
+	ticket_array.each do |n|
+		if n == winner_array[counter]
+			matches += 1
+		end
+		counter += 1
+	end
+	puts matches
 	
 end
 
-off('123456')
+off('123456', '123457')
