4dc8ebdf15586eb0722b105c5730ba00221ce50c
Daniel Parks <dparks715@yahoo.com>
2017-01-28 17:25:35 -0500

    no matches, false

--- test_off_by_one.rb
+++ test_off_by_one.rb
@@ -7,4 +7,9 @@ class TestStringsOffbyOne < Minitest::Test
 		results = off('1234','1234')
 		assert_equal(false, results)
 	end
+
+	def test_no_matches_false
+		results = off('1234','5678')
+		assert_equal(false, results)
+	end
 end
