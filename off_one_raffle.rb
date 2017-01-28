1bd373ba6119f507e7b0828a2b3640fa94a2e5f3
Daniel Parks <dparks715@yahoo.com>
2017-01-28 18:38:30 -0500

    function 1st build, moving to tests

--- off_one_raffle.rb
+++ off_one_raffle.rb
@@ -2,16 +2,34 @@
 #put the numbers from the array that are one off from our number into a new array and return it
 
 def off_one(ticket,winners)
+	off_by_one = []
 	strings = []
+	our_num = ticket.split("")
 	#taking each element in winning numbers, splitting them individually
 	#pushing them into array called strings to create multidimensional array
 	winners.each do |n|
 		strings << n.split("")
 	end
-	#printing to make sure it does what I think it does
-	print strings
+	#iterates through each subarray using 2 loops
+	#uses 2 counters, count to iterate through the subarray indices
+	#matches to keep track of digits that match between our number and the winning
+	#number that we are currently iterating through
+	strings.each do |num|
+		matches = 0
+		count = 0
 
-
+		num.each do |n|
+			
+			if n == our_num[count]
+				matches += 1
+			end
+			count += 1
+		end
+		#if our num length minus matches = 1, we know its off by one
+		#so we push it into the array
+		if num.length - matches == 1
+			off_by_one << num
+		end
+	end
+off_by_one
 end
-
-off_one(1, ['1234', '2345', '3456', '4567'])
