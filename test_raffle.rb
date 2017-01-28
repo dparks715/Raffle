9677fbd94324ec11f65d8774fe42500d69ce41eb
Daniel Parks <dparks715@yahoo.com>
2017-01-28 16:21:24 -0500

    test for 3 element array, true

--- test_raffle.rb
+++ test_raffle.rb
@@ -4,9 +4,15 @@ require_relative 'raffle.rb'
 class TestRaffleFunction < Minitest::Test
 
 	def test_empty
+		#using and empty array to make sure it returns false
 		empty_array = []
 		assert_equal(false, raffle(1234,empty_array))
-		
+	end
+
+	def test_three_num_true
+		#setting results to our function plus values for cleaner assert_equal
+		results = raffle(1234, [2345, 4657, 1234])
+		assert_equal(true, results)
 	end
 
 end
