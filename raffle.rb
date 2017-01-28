94771d18b77e44080880cdbf3591f1cd3f0303aa
Daniel Parks <dparks715@yahoo.com>
2017-01-28 16:11:54 -0500

    added comments

--- raffle.rb
+++ raffle.rb
@@ -1,5 +1,7 @@
+#two parameters, one for our ticket, and one for the array of winners
 def raffle(ticket, winner)
-	
+	#each loop, iterates through array, checks if each element is equal to ticket
+	#if true it will print our ticket
 	winner.each do |num|
 		if num == ticket
 			puts num
