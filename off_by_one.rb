f53dc8791e672593f62643f3e712f5d4bdc00e3c
Daniel Parks <dparks715@yahoo.com>
2017-01-28 17:19:21 -0500

    function complete, moving to testing

--- off_by_one.rb
+++ off_by_one.rb
@@ -1,8 +1,9 @@
 def off(ticket, winner)
-	#checking the class after using split.  It returns and array
+	#splits our strings individually by character and puts them into arrays
 	ticket_array = ticket.split("")
 	winner_array = winner.split("")
 	
+	#iterates through the arrays, checking each index position against each other
 	counter = 0
 	matches = 0
 	ticket_array.each do |n|
@@ -11,8 +12,8 @@ def off(ticket, winner)
 		end
 		counter += 1
 	end
-	puts matches
-	
-end
+	#takes the length of our array and subtracts the number of matches
+	#using this method we can provide tickets of any length
+	puts ticket_array.length - matches == 1
 
-off('123456', '123457')
+end
